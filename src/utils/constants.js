1f78dbe7b661b2ef0370c4f9f8e99f5b085cc694
Sharon <sharon.peishan.kennedy@gmail.com>
2018-05-04 13:59:03 -0400

    add filter forms for learning circles search, make ApiHelper compatible with p2pu-website

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -24,36 +24,41 @@ export const SEARCH_PROPS = {
   }
 };
 
+export const DEFAULT_ORIGIN = 'https://learningcircles.p2pu.org'
+
 export const API_ENDPOINTS = {
-  learningCircle: '/api/learning-circle/',
-  registration: '/en/accounts/fe/register/',
-  login: '/en/accounts/fe/login/',
+  learningCircle: `/api/learning-circle/`,
+  registration: `/en/accounts/fe/register/`,
+  login: `/en/accounts/fe/login/`,
   learningCircles: {
-    postUrl: '/api/learning-circle/',
-    baseUrl: '/api/learningcircles/?',
-    searchParams: ['q', 'topics', 'weekdays', 'latitude', 'longitude', 'distance', 'active', 'limit', 'offset', 'city', 'signup', 'team_id', 'id']
+    postUrl: `/api/learning-circle/`,
+    baseUrl: `/api/learningcircles/?`,
+    searchParams: ['q', 'topics', 'weekdays', 'latitude', 'longitude', 'distance', 'active', 'limit', 'offset', 'city', 'signup', 'team_id']
   },
   courses: {
-    baseUrl: '/api/courses/?',
-    searchParams: ['q', 'topics', 'order', 'course_id']
+    baseUrl: `/api/courses/?`,
+    searchParams: ['q', 'topics', 'order']
   },
   learningCirclesTopics: {
-    baseUrl: 'https://learningcircles.p2pu.org/api/learningcircles/topics/?',
+    baseUrl: `/api/learningcircles/topics/?`,
     searchParams: []
   },
   coursesTopics: {
-    baseUrl: 'https://learningcircles.p2pu.org/api/courses/topics/?',
+    baseUrl: `/api/courses/topics/?`,
     searchParams: []
   },
   stats: {
-    baseUrl: 'https://learningcircles.p2pu.org/api/landing-page-stats/?',
+    baseUrl: `/api/landing-page-stats/?`,
     searchParams: []
   },
   landingPage: {
-    baseUrl: 'https://learningcircles.p2pu.org/api/landing-page-learning-circles/?',
+    baseUrl: `/api/landing-page-learning-circles/?`,
     searchParams: []
   },
+  whoami: {
+    baseUrl: `/en/accounts/fe/whoami/`
+  },
   images: {
-    postUrl: '/api/upload_image/'
+    postUrl: `/api/upload_image/`
   }
 };
