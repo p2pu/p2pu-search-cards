e908dbe33da31f6ed51d98d135852103d8af862f
Sharon <sharon.peishan.kennedy@gmail.com>
2018-09-10 15:57:50 -0400

    umd build and default colour for cards

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = (props) => {
   return (
-    <div className={`result-item grid-item col-md-4 col-sm-12 col-xs-12 ${props.colorClass}`}>
+    <div className={`result-item grid-item ${props.colorClass}`}>
       <div className="card">
         {props.children}
       </div>
