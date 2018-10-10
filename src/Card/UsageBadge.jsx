28630855df10c1d24a6c00cbcb996a5b60e0ef69
Dirk Uys <dirkcuys@gmail.com>
2018-10-10 17:20:55 +0200

    Remove react-tooltip

--- src/Card/UsageBadge.jsx
+++ src/Card/UsageBadge.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactTooltip from 'react-tooltip'
 
 const UsageBadge = ({ number, id }) => {
   const display = number > 0
@@ -12,9 +11,6 @@ const UsageBadge = ({ number, id }) => {
       <div className='usage-badge' data-tip data-for={id}>
         <div className='text'>
           <i className="material-icons">{icon}</i>{number}
-          <ReactTooltip id={id} class="p2pu-tooltip" place="bottom" effect="solid" aria-haspopup='true'>
-            <span>{tooltipText}</span>
-          </ReactTooltip>
         </div>
       </div>
     );
