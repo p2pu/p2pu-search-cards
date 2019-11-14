d326614a0ab337c447b6e87e282e72e05f1ac540
Dirk Uys <dirkcuys@gmail.com>
2019-11-14 17:21:30 +0200

    Update signup form

--- src/LearningCircleSignup/SignupForm.jsx
+++ src/LearningCircleSignup/SignupForm.jsx
@@ -4,33 +4,7 @@ import Promise from 'promise-polyfill'
 import 'whatwg-fetch'
 import {InputWithLabel} from 'p2pu-input-fields';
 import {CheckboxWithLabel} from 'p2pu-input-fields';
-import SelectWithLabel from './select-with-label';
 import SignupSuccess from './SignupSuccess';
-
-const GOAL_OPTIONS = [
-  { value: `To increase my employability`, label: t`To increase my employability`},
-  { value: `Professional development for my current job`, label:  t`Professional development for my current job`},
-  { value: `To accompany other educational programs`, label:  t`To accompany other educational programs`},
-  { value: `Personal interest`, label:  t`Personal interest`},
-  { value: `Social reasons`, label:  t`Social reasons`},
-  { value: `For fun / to try something new`, label:  t`For fun / to try something new`},
-  { value: `Other`, label: t`Other`},
-];
-
-const COMPUTER_ACCESS_OPTIONS = [
-  { value: `Both`, label: t`Both`},
-  { value: `Just a laptop`, label: t`Just a laptop`},
-  { value: `Just headphones`, label: t`Just headphones`},
-  { value: `Neither`, label: t`Neither`},
-];
-
-const DIGITAL_LITERACY_OPTIONS = [
-  { value: '0', label: t`Can't do`},
-  { value: '1', label: t`Need help doing`},
-  { value: '2', label: t`Can do with difficulty`},
-  { value: '3', label: t`Can do`},
-  { value: '4', label: t`Expert (can teach others)`},
-]
 
 export default class SignupForm extends React.Component {
 
@@ -118,11 +92,10 @@ export default class SignupForm extends React.Component {
     const {
       name,
       email,
+      mobile,
       goals,
       support,
-      computer_access,
-      use_internet,
-      mobile,
+      consent,
       communications_opt_in
     } = this.state;
     return (
@@ -143,49 +116,10 @@ export default class SignupForm extends React.Component {
                 label={t`Email address`}
                 value={email}
                 handleChange={this.onDataChange}
-                name={'email'}
-                id={'id_email'}
+                name='email'
+                id='id_email'
                 errorMessage={this.getError('email')}
                 required={true}
-              />
-              <SelectWithLabel
-                label={t`What is your goal for taking this learning circle?`}
-                classes="no-flex form-group"
-                name={'goals'}
-                value={goals}
-                options={GOAL_OPTIONS}
-                handleChange={this.onDataChange}
-                placeholder={t`Select one of the following`}
-                multi={false}
-              />
-              <InputWithLabel
-                label={t`A successful study group requires the support of all of its members. How will you help your peers achieve their goals?`}
-                value={support}
-                handleChange={this.onDataChange}
-                name={'support'}
-                id={'id_support'}
-                errorMessage={this.state.errors && this.state.errors.signup_questions && this.state.errors.signup_questions[0].support}
-                required={true}
-              />
-              <SelectWithLabel
-                label={t`Can you bring a laptop and headphones to the learning circle each week?`}
-                classes='no-flex form-group'
-                options={COMPUTER_ACCESS_OPTIONS}
-                multi={false}
-                value={computer_access}
-                name='computer_access'
-                handleChange={this.onDataChange}
-                placeholder={t`Select one of the following`}
-              />
-              <SelectWithLabel
-                label={t`How comfortable are you using the internet?`}
-                classes='no-flex form-group'
-                options={DIGITAL_LITERACY_OPTIONS}
-                multi={false}
-                value={use_internet}
-                name='use_internet'
-                handleChange={this.onDataChange}
-                placeholder={t`Select one of the following`}
               />
               <InputWithLabel
                 label={t`If you'd like to receive weekly text messages reminding you of upcoming learning circle meetings, put your phone number here.`}
@@ -197,9 +131,37 @@ export default class SignupForm extends React.Component {
                 required={false}
               />
               <p>{t`Your number won't be shared with other participants.`}</p>
+              <InputWithLabel
+                label={t`Why do you want to learn this topic?`}
+                name='goals'
+                value={goals}
+                handleChange={this.onDataChange}
+                id='id_email'
+                errorMessage={this.getError('goals')}
+                required={true}
+              />
+              <InputWithLabel
+                label={t`A successful study group requires the support of all of its members. How will you help your peers achieve their goals?`}
+                value={support}
+                handleChange={this.onDataChange}
+                name={'support'}
+                id={'id_support'}
+                errorMessage={this.state.errors && this.state.errors.signup_questions && this.state.errors.signup_questions[0].support}
+                required={true}
+              />
               <CheckboxWithLabel
                 classes="d-flex"
-                label={t`Would you like to receive information about other learning opportunities in the future?`}
+                label={t`I give consent that P2PU can share my signup info with the learning circle facilitator and send me info regarding the learning circle.`}
+                value={consent}
+                handleChange={this.onDataChange}
+                name='consent'
+                id={'id_consent'}
+                errorMessage={''}
+                required={false}
+              />
+              <CheckboxWithLabel
+                classes="d-flex"
+                label={t`I would like to receive information about other learning opportunities in the future.`}
                 value={communications_opt_in}
                 handleChange={this.onDataChange}
                 name={'communications_opt_in'}
@@ -207,7 +169,8 @@ export default class SignupForm extends React.Component {
                 errorMessage={''}
                 required={false}
               />
-              <button className="p2pu-btn blue" onClick={this.onSubmit}>{t`Sign up`}</button>
+              {this.state.consent && <button className="p2pu-btn blue" onClick={this.onSubmit}>{t`Sign up`}</button>}
+              {!this.state.consent && <button className="p2pu-btn blue" disabled>{t`Sign up`}</button>}
             </div>
         }
         <button className="p2pu-btn blue secondary" onClick={this.props.onCancel}>{t`Back to search`}</button>
