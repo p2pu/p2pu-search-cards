4a1f0d01f444f2a8232fcb8f96b5b6f9d04b2a3b
Sharon Kennedy <sharon@nomadiclabs.ca>
2020-04-26 01:31:38 -0400

    customizable component when no results are available on initial query

--- demo/src/index.js
+++ demo/src/index.js
@@ -24,6 +24,7 @@ class App extends React.Component {
         Browse={BrowseLearningCircles}
         onSelectResult={handleSelectResult}
         initialState={{ languages: ['en'] }}
+        origin={'http://localhost:8000'}
       />
     </div>
     );
